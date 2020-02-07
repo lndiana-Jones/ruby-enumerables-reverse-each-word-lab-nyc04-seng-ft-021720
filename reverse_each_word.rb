2dc2c877a9f73e1d9365f79bff19236ea533cda8
Andrew D'Antonio <andrewdantonio10@gmail.com>
2020-02-07 19:54:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,7 @@ def reverse_each_word(string)
   words_arr = string.split(" ")
  
   
-  words_arr.collect {|ele| 
-    ele.reverse
-  end
+  words_arr.collect {|ele| ele.reverse
   
   words_arr.join(" ")
     
